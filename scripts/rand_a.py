066638dd082595e28d77ab657f5d7e9ae58db222
shinnosuke tamai <76773901+ShinnosukeTamai@users.noreply.github.com>
2021-01-21 01:05:41 +0900

    Create rand_a.py

---
+++ scripts/rand_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#
+#                                                                                                                                                            #                                                                                                                                                            
+
+
+import rospy
+import random
+import string
+from std_msgs.msg import Int32
+
+rospy.init_node("prac")
+pub = rospy.Publisher('prac_up', Int32, queue_size=1)
+dat = string.ascii_lowercase#+string.ascii_uppercase
+
+T1 = 0
+T2 = 0
+NOT = 0
+while NOT < 10:
+    NOT += 1
+    dat1=''.join([random.choice(dat) for i in range(6)])
+    print(dat1)
+    ans = input("GO!:")
+
+
+    if dat1 == ans :
+        print("True")
+        T1 += 1
+    else :
+        print("False")
+        T2 += 1
+
+
+print("finsh! true:", T1," number of times:", NOT)
